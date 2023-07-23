04281aba4ca9b2dd957ea8bc728e4606538a68b8
Ana Rodriguez Reiner <arodriguezreiner@outlook.com>
2023-07-22 18:07:16 -0700

    commit lab

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Add your code here
+// const formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json",
+//   },
+//   body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+//   })
+//   .catch(function (error){
+//     alert("Bad things! Ragnarok!");
+//     console.log(error.message)
+//   })
+
+function submitData(name, email) {
+  const data = {
+  name,
+  email
+};
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify(data),
+};
+
+return fetch("http://localhost:3000/users", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    document.body.innerHTML = object.id
+  })
+  .catch(function (error) {
+    document.body.innerHTML = error.message;
+  });
+}
